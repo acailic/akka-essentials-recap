7a63ab8d25c4f9d2eb6f3bf5642602afe2841673
a.ilic <a.ilic@levi9.com>
2020-07-02 20:03:49 +0200

    recap

--- src/main/scala/part1recap/MultithreadingRecap.scala
+++ src/main/scala/part1recap/MultithreadingRecap.scala
@@ -1,5 +1,8 @@
 package part1recap
 
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
 object MultithreadingRecap extends App {
   // creating threads in JVM
   val aThread = new Thread(new Runnable {
@@ -18,13 +21,53 @@ object MultithreadingRecap extends App {
 
   // different runs produce different results
 
-  class BankAccount(private var amount: Int){
-    override def toString: String = ""+amount
-    def withdraw (money: Int) =this.amount -= money
+  //making it thread safe
+  //1 synchronized
+  //2 volatile -- no right and write simult but for specific type
+  class BankAccount(@volatile private var amount: Int) {
+    override def toString: String = "" + amount
+
+    def withdraw(money: Int) = this.amount -= money
+
+    def safeWithdraw(money: Int) = this.synchronized {
+      this.amount -= money
+    }
   }
 
   // not thread safe .
+  // NOT ATOMIC
+  // adding synchronized blocks
+  //
 
+  // inter thread communication
+  // wait and notify
+  // scala future
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  var future = Future {
+    // long computation - on a different thread
+    32
+  }
+
+  // we can do callbacks
+  future.onComplete {
+    case Success(32) => println("Meaning of life .............")
+    case Failure(_) => println("errrrrrorr  ")
+  }
+  /// future is ammoniated contract it has functional primitive
+
+  val aProccesedFuture = future.flatMap {
+    value => Future(value + 2)
+  } // Future with 34
+
+
+  val filteredFuture = future.filter(_ % 2 == 0) // if no elements  there is NoSuchElementException
+
+  // for comprehension
+  val aNonSenseFuture = for {
+    meaningOfLife <- future
+    filteredMeaning <- filteredFuture
+  } yield meaningOfLife + filteredMeaning
 
 
 }
