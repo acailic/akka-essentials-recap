911a2dd505ac2d7f59f9e9fa7c424a2097739cdf
a.ilic <a.ilic@levi9.com>
2020-07-02 20:58:03 +0200

    recap

--- src/main/scala/part1recap/ThreadModelLimitations.scala
+++ src/main/scala/part1recap/ThreadModelLimitations.scala
@@ -1,5 +1,7 @@
 package part1recap
 
+import scala.concurrent.Future
+
 object ThreadModelLimitations extends App {
 
   /* 1.
@@ -75,5 +77,21 @@ object ThreadModelLimitations extends App {
   delegateToBackgroundThread(() => println(" this should run in the background "))
 
 
+
+  /*  3.
+    Delegate tracing dealing with errors in a multithreaded env
+   */
+  // 1 milion numbers in 10 threads
+  import scala.concurrent.ExecutionContext.Implicits.global
+  val  futures = (0 to 9)
+    .map(i=> 100000* i  until 100000 * (i +1)) //0-99999, 100000-199999, 200000-299999
+    .map(range => Future {
+        if(range.contains(546735)) throw new RuntimeException("invalid number")  // example so it fails one future
+        range.sum
+
+  })
+
+  val sumFuture = Future.reduceLeft(futures)(_+_)  //Future  with sum  of all numbers
+  sumFuture.onComplete(println)
 
 }
