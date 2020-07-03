6695819d7145faeed3f5d451a5f1e4be9697e071
a.ilic <a.ilic@levi9.com>
2020-07-03 13:01:13 +0200

    ChildActors

--- src/main/scala/part2actors/ChildActors.scala
+++ src/main/scala/part2actors/ChildActors.scala
@@ -1,22 +1,50 @@
 package part2actors
 
-import akka.actor.Actor
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 
-class ChildActors extends  App {
+object ChildActors extends App {
 
   // Actors can create other actors
-  object Parent{
+  object Parent {
+
     case class CreateChild(name: String)
+
     case class TellChild(message: String)
-  }
-  class Parent extends Actor {
-    override def receive: Receive = ???
+
   }
 
-  class Child extends Actor{
+  class Parent extends Actor {
+
+    import Parent._
+
+    var child: ActorRef = null
+
+
+    override def receive: Receive = {
+      case CreateChild(name) =>
+        println(s"${self.path} creating child")
+        //create a new actor here
+        var childRef = context.actorOf(Props[Child], name)
+        context.become(withChild(childRef)) //child = childRef
+    }
+
+    def withChild(childRef: ActorRef): Receive = {
+      case TellChild(message) =>
+        if (childRef != null) childRef forward message
+    }
+  }
+
+  class Child extends Actor {
     override def receive: Receive = {
       case message => println(s"${self.path} : here is the message $message")
     }
   }
+
+
+  import Parent._
+  val system = ActorSystem("demoActorSystem")
+  val parent = system.actorOf(Props[Parent],"parent")
+  parent ! CreateChild("ivica")
+  parent ! TellChild("desi sinovac")
 
 }
