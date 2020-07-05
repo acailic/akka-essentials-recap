88b194dd5cf5ae88f2e807eea4af733ff698ad93
a.ilic <a.ilic@levi9.com>
2020-07-06 01:41:51 +0200

    StartingStoppingActors.scala

--- src/main/scala/part4faulttolerance/StartingStoppingActors.scala
+++ src/main/scala/part4faulttolerance/StartingStoppingActors.scala
@@ -1,8 +1,44 @@
 package part4faulttolerance
 
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+
 object StartingStoppingActors extends App {
 
+  val system = ActorSystem("DemoStartingStoppingActors")
+
+  object Parent{
+    case class StartChild(name: String)
+    case class StopChild(name: String)
+    case object Stop
+  }
+
+  class Parent extends Actor with ActorLogging  {
+    import Parent._
+
+    override def receive: Receive = withChildren(Map())
+
+    def withChildren(children: Map [String,ActorRef]):Receive = {
+      case StartChild(name) =>
+        log.info(s"Starting child $name")
+        context.become(withChildren(children+(name->context.actorOf(Props[Child],name))))
+      case StopChild(name) =>
+        log.info(s"Stopping child $name")
+        val childOption= children.get(name)
+        childOption.foreach(childRef=> context.stop(childRef))
+    }
+  }
+
+  class Child extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(message.toString)
+    }
+  }
 
 
+  import Parent._
+  val parent = system.actorOf(Props[Parent],"parent")
+  parent ! StartChild("child1")
+  val child = system.actorSelection("/user/parent/child1")
+  child ! "Hi kid"
 
 }
