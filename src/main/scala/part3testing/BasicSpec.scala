98d6249681c1f064d5b5f2006ed98209674208f0
a.ilic <a.ilic@levi9.com>
2020-07-06 02:56:07 +0200

    BasicSpec.scala

--- src/main/scala/part3testing/BasicSpec.scala
+++ src/main/scala/part3testing/BasicSpec.scala
@@ -4,6 +4,8 @@ import akka.actor.{Actor, ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
+import scala.concurrent.duration._
+
 class BasicSpec extends TestKit(ActorSystem("BasicSpec"))
   with ImplicitSender
   with WordSpecLike
@@ -27,8 +29,28 @@ class BasicSpec extends TestKit(ActorSystem("BasicSpec"))
       val echoActor = system.actorOf(Props[SimpleActor])
       val message = "hello test"
       echoActor ! message
-      expectMsg(message)
+      expectMsg(message) // akka.test.single-expect-default
+      //testActor is behind these messages
+    }
+  }
 
+  "a black hole actor" should {
+    " send back message " in {
+      val blackHole = system.actorOf(Props[BlackHoleActor])
+      val message = "hello test"
+      blackHole ! message
+      expectNoMessage(1 second)
+    }
+  }
+  //message assertions
+
+  "a lab test actor" should {
+    val labTestActor = system.actorOf(Props[LabTestActor])
+    " send back uppercase " in {
+
+      labTestActor ! "i love akka"
+      val reply = expectMsgType[String]
+      assert(reply=="I LOVE AKKA")
     }
   }
 }
@@ -41,5 +63,14 @@ object BasicSpec {
     }
   }
 
+  class BlackHoleActor extends Actor {
+    override def receive: Receive = Actor.emptyBehavior
+  }
+
+  class LabTestActor extends Actor {
+    override def receive: Receive = {
+      case message:String => sender() ! message.toUpperCase()
+    }
+  }
 }
 
