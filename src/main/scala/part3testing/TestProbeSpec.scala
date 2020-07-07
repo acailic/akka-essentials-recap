3829bdaccb646c257bbc5c5090f723bcbcbdf974
a.ilic <a.ilic@levi9.com>
2020-07-07 23:15:51 +0200

    TestProbeSpec.scala

--- src/main/scala/part3testing/TestProbeSpec.scala
+++ src/main/scala/part3testing/TestProbeSpec.scala
@@ -1,6 +1,6 @@
 package part3testing
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorRef, ActorSystem}
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
@@ -16,9 +16,45 @@ class TestProbeSpec extends TestKit(ActorSystem("TestProbeSpec"))
 
 
 object TestProbeSpec {
+
   /// scenario
   /*
+    word counting hierarchy master slave
+
+    send some work to master
+    -   master send to slave piece work to
+    -   slave process to work and replies to master
+    -   master aggregates the result
+    -   master send total count  of the original requester
 
    */
+
+  case class Register(slaveRef: ActorRef)
+
+  case class SlaveWork(text: String, originalRequestor: ActorRef)
+
+  case class WorkCompleted(count: Int, originalRequestor: ActorRef)
+
+  case class Work(text: String)
+
+  case class Report(number: Int)
+
+
+  class Master extends Actor {
+    override def receive: Receive = {
+      case Register(slaveRef) => context.become(onLine(slaveRef, 0))
+      case _ => ///ignore
+
+    }
+
+    def onLine(slaveRef: ActorRef, totalWordCount: Int): Receive = {
+      case Work(text) => slaveRef ! SlaveWork(text, sender())
+      case WorkCompleted(count, originalRequestor) =>
+        val newTotalWordCount = totalWordCount + count
+        originalRequestor ! Report(newTotalWordCount)
+        context.become(onLine(slaveRef, newTotalWordCount))
+    }
+  }
+
 
 }
