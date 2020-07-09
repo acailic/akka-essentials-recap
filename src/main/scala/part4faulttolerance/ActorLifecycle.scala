73378ec2260b72ad2d40793aeb0c6f171bf22ecc
a.ilic <a.ilic@levi9.com>
2020-07-09 20:14:51 +0200

    ActorLifecycle.scala

--- src/main/scala/part4faulttolerance/ActorLifecycle.scala
+++ src/main/scala/part4faulttolerance/ActorLifecycle.scala
@@ -22,5 +22,50 @@ object ActorLifecycle extends App {
   parent ! StartChild
   parent ! PoisonPill
 
+  /*
+      restart
+   */
+
+  object Fail
+  object Check
+
+  object FailChild
+  object CheckChild
+
+  class Parent extends Actor with ActorLogging {
+    private val child = context.actorOf(Props[Child], "supervisedChild")
+
+    override def receive: Receive = {
+      case FailChild => child ! Fail
+      case CheckChild => child ! Check
+    }
+  }
+
+  class Child extends Actor with ActorLogging {
+    override def preStart(): Unit = log.info("supervised child started")
+
+    override def postStop(): Unit = log.info("supervised child stopped")
+
+    override def preRestart(reason: Throwable, message: Option[Any]): Unit =
+      log.info(s"supervised actor restarting license because of ${reason.getMessage}")
+
+
+    override def postRestart(reason: Throwable): Unit =
+      log.info(s"supervised actor restarted")
+
+
+    override def receive: Receive = {
+      case Fail =>
+        log.warning("child will fail now")
+        throw new RuntimeException("I failed")
+      case Check =>
+        log.info("alive and kicking")
+    }
+  }
+
+  val supervisor = system.actorOf(Props[Parent], "supervisor")
+  supervisor ! FailChild  // child
+  supervisor ! CheckChild  // child
+
 }
 
