2485ffaf732c394b0ab028820df5c211773f3359
a.ilic <a.ilic@levi9.com>
2020-07-09 08:54:12 +0200

    ActorLifecycle.scala

---
+++ src/main/scala/part4faulttolerance/ActorLifecycle.scala
@@ -0,0 +1,12 @@
+package part4faulttolerance
+
+import akka.actor.{Actor, ActorLogging}
+
+object ActorLifecycle extends App {
+
+
+    class LifecycleActor extends Actor with ActorLogging{
+      override def receive : Receive ={}
+      case
+    }
+}
