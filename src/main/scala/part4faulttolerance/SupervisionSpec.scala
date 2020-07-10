b90ff02847678ecc2a2d28ebf05b46a3ea6677fd
a.ilic <a.ilic@levi9.com>
2020-07-11 01:56:02 +0200

    SupervisionSpec.scala

--- src/main/scala/part4faulttolerance/SupervisionSpec.scala
+++ src/main/scala/part4faulttolerance/SupervisionSpec.scala
@@ -1,14 +1,73 @@
 package part4faulttolerance
 
 import akka.actor.SupervisorStrategy.{Escalate, Restart, Resume, Stop}
-import akka.actor.{Actor, OneForOneStrategy, Props}
+import akka.actor.{Actor, ActorRef, ActorSystem, OneForOneStrategy, Props, Terminated}
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
-class SupervisionSpec extends TestKit("SupervisionSpec")
+class SupervisionSpec extends TestKit(ActorSystem("SupervisionSpec"))
   with ImplicitSender with WordSpecLike with BeforeAndAfterAll {
   override def afterAll(): Unit = {
     TestKit.shutdownActorSystem(system)
+  }
+
+  import SupervisionSpec._
+  "A supervision " should {
+    "a resume its child in case of minor error" in {
+      val supervisor=system.actorOf(Props[Supervisor])
+      supervisor ! Props[FuzzyWordCount]
+      val child = expectMsgType[ActorRef]
+
+      child ! "I love akka"
+      child ! Report
+      expectMsg(3)
+
+      child ! "Aaaa laaaaove aaaasdadsa sdadasd sdadsad  "
+      child ! Report
+      expectMsg(3)
+
+    }
+  }
+
+  "A supervision " should {
+    "restart child in case of empty sentence " in {
+      val supervisor=system.actorOf(Props[Supervisor])
+      supervisor ! Props[FuzzyWordCount]
+      val child = expectMsgType[ActorRef]
+
+      child ! ""
+      child ! Report
+      expectMsg(0)
+
+    }
+  }
+
+  "A supervision " should {
+    "terminate child in case of error" in {
+      val supervisor=system.actorOf(Props[Supervisor])
+      supervisor ! Props[FuzzyWordCount]
+      val child = expectMsgType[ActorRef]
+
+      watch(child)
+      child ! "akka is nice"
+      val terminatedMessage= expectMsgType[Terminated]
+      assert(terminatedMessage.actor==child)
+
+    }
+  }
+
+  "A supervision " should {
+    "escalate on exception" in {
+      val supervisor=system.actorOf(Props[Supervisor])
+      supervisor ! Props[FuzzyWordCount]
+      val child = expectMsgType[ActorRef]
+
+      watch(child)
+      child ! "123123213"
+      val terminatedMessage= expectMsgType[Terminated]
+      assert(terminatedMessage.actor==child)
+
+    }
   }
 
 }
@@ -32,11 +91,12 @@ object SupervisionSpec {
     }
   }
 
-
+  case object Report
   class FuzzyWordCount extends Actor {
     var words = 0
 
     override def receive: Receive = {
+      case Report=>  sender() ! words
       case "" => throw new NullPointerException("sentences empty")
       case sentence: String =>
         if (sentence.length > 20) throw new RuntimeException("sentence is big")
