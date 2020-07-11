3996c652ed5bd78eeb6a213ba1c8548cca98329b
a.ilic <a.ilic@levi9.com>
2020-07-11 16:46:57 +0200

    Mailboxes.scala

--- src/main/scala/part5infra/Mailboxes.scala
+++ src/main/scala/part5infra/Mailboxes.scala
@@ -1,11 +1,49 @@
 package part5infra
 
-import akka.actor.ActorSystem
+import akka.actor.{Actor, ActorLogging, ActorSystem, PoisonPill, Props}
+import akka.dispatch.{PriorityGenerator, UnboundedPriorityMailbox}
+import com.typesafe.config.Config
 
-object Mailboxes  extends App {
+object Mailboxes extends App {
 
   val system = ActorSystem("MailboxDemo")
 
+  class SimpleActor extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(message.toString)
+    }
+
+  }
+
+  /*
+    Interesting case #1 - custom priority mailbox
+    P0 -> most important
+    P1
+    P2
+    P3
+   */
+
+  // step 1 mailbox definition
+  class SupportTicketPriorityMailbox(settings: ActorSystem.Settings, config: Config)
+    extends UnboundedPriorityMailbox(
+      PriorityGenerator {
+        case message: String if message.startsWith("[P0]") => 0
+        case message: String if message.startsWith("[P1]") => 1
+        case message: String if message.startsWith("[P2]") => 2
+        case message: String if message.startsWith("[P3]") => 3
+        case _ => 4
+      }
+    )
+
+    // step 2 mailbox definition into config
+    // step 3 attach  the dispatcher to actor
+
+    val supportTicketLogger = system.actorOf(Props[SimpleActor].withDispatcher("support-ticket-dispatcher"))
+    supportTicketLogger ! PoisonPill
+    //Thread.sleep(1000)
+    supportTicketLogger ! "[P3] this thing would be nice to have"
+    supportTicketLogger ! "[P0] solve it now"
+    supportTicketLogger ! "[P1] do this when you have time"
 
 
 
