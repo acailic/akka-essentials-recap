fe1de2fab92925d3b547aa035b0bb5584c67c43f
a.ilic <a.ilic@levi9.com>
2020-07-14 01:59:16 +0200

    TimersSchedulers.scala

--- src/main/scala/part5infra/TimersSchedulers.scala
+++ src/main/scala/part5infra/TimersSchedulers.scala
@@ -1,5 +1,30 @@
 package part5infra
 
-object TimersSchedulers {
+import akka.actor.{Actor, ActorLogging, ActorSystem, Cancellable, Props}
 
+object TimersSchedulers extends App {
+
+  class SimpleActor extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(message.toString)
+    }
+
+
+  }
+
+  val system = new ActorSystem("SchedulersTimeDemo")
+  val simpleActor = system.actorOf(Props[SimpleActor])
+
+  system.log.info("Schedulers reminder to simple actor")
+
+  import system.dispatcher
+
+  system.scheduler.scheduleOnce(1 second)
+  {
+    simpleActor ! "reminder"
+  }
+
+  val routine ! Cancellable = system.scheduler.schedule(1 second, 2 second){
+    simpleActor ! "heartbeat"
+  }
 }
