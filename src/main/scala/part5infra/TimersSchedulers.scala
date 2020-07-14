103d1749c900c7b5329a5b5582d0082d3734b73b
a.ilic <a.ilic@levi9.com>
2020-07-14 02:20:36 +0200

    TimersSchedulers.scala

--- src/main/scala/part5infra/TimersSchedulers.scala
+++ src/main/scala/part5infra/TimersSchedulers.scala
@@ -1,6 +1,7 @@
 package part5infra
 
 import akka.actor.{Actor, ActorLogging, ActorSystem, Cancellable, Props}
+
 import scala.concurrent.duration._
 
 object TimersSchedulers extends App {
@@ -11,26 +12,27 @@ object TimersSchedulers extends App {
     }
   }
 
-  val system =   ActorSystem("SchedulersTimeDemo")
+  val system = ActorSystem("SchedulersTimeDemo")
   val simpleActor = system.actorOf(Props[SimpleActor])
 
   system.log.info("Schedulers reminder to simple actor")
 
   import system.dispatcher
   // imports context implicitly
-  system.scheduler.scheduleOnce(1 second)
-  {
-    simpleActor ! "reminder"
-  }
+/*
+     system.scheduler.scheduleOnce(1 second) {
+      simpleActor ! "reminder"
+    }
 
-  // heartbeat
-  val routine: Cancellable = system.scheduler.schedule(1 second, 2 second){
-    simpleActor ! "heartbeat"
-  }
+    // heartbeat
+    val routine: Cancellable = system.scheduler.schedule(1 second, 2 second) {
+      simpleActor ! "heartbeat"
+    }
 
-  system.scheduler.scheduleOnce(5 seconds){
-    routine.cancel()
-  }
+    system.scheduler.scheduleOnce(5 seconds) {
+      routine.cancel()
+    }
+*/
 
   /// not use ref in sched
   /// schedulers are not for months messages
@@ -42,7 +44,38 @@ object TimersSchedulers extends App {
   3-if send another mess, time windows is reset
    */
 
+  class SelfClosingActor extends Actor with ActorLogging {
+    var schedule = createTimeoutWindow()
+
+    def createTimeoutWindow(): Cancellable = {
+      context.system.scheduler.scheduleOnce(1 second) {
+        self ! "timeout"
+      }
+    }
 
 
+    override def receive: Receive = {
+      case "timeout" =>
+        log.info("Stopping myself")
+        context.stop(self)
+      case message =>
+        log.info(s"Received $message, staying alive")
+        schedule.cancel()
+        schedule = createTimeoutWindow()
+    }
+  }
+
+  val selfClosingActor = system.actorOf(Props[SelfClosingActor])
+  system.scheduler.scheduleOnce(250 millis) {
+    selfClosingActor ! "ping"
+  }
+
+  system.scheduler.scheduleOnce(2 seconds) {
+    system.log.info("sending pong message")
+    selfClosingActor ! "pong" //will never reach
+  }
+
+  class TimerBasedSCActor extends Actor with ActorLogging {
+  }
 
 }
