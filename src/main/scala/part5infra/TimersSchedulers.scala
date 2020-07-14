1e459b53bd82d54bac3a365f7b7b21d6d4a02d05
a.ilic <a.ilic@levi9.com>
2020-07-14 02:01:08 +0200

    TimersSchedulers.scala

--- src/main/scala/part5infra/TimersSchedulers.scala
+++ src/main/scala/part5infra/TimersSchedulers.scala
@@ -1,7 +1,7 @@
 package part5infra
 
 import akka.actor.{Actor, ActorLogging, ActorSystem, Cancellable, Props}
-
+import scala.concurrent.duration._
 object TimersSchedulers extends App {
 
   class SimpleActor extends Actor with ActorLogging {
@@ -18,13 +18,18 @@ object TimersSchedulers extends App {
   system.log.info("Schedulers reminder to simple actor")
 
   import system.dispatcher
-
+  // imports context implicitly
   system.scheduler.scheduleOnce(1 second)
   {
     simpleActor ! "reminder"
   }
 
+  // heartbeat
   val routine ! Cancellable = system.scheduler.schedule(1 second, 2 second){
     simpleActor ! "heartbeat"
   }
+
+
+
+
 }
