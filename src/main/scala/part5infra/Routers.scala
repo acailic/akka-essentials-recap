7162523876123bf7f12a35b62d401d0786528a9c
a.ilic <a.ilic@levi9.com>
2020-07-15 00:01:37 +0200

    Routers.scala

--- src/main/scala/part5infra/Routers.scala
+++ src/main/scala/part5infra/Routers.scala
@@ -1,5 +1,38 @@
 package part5infra
 
-object Routers {
+import akka.actor.{Actor, ActorLogging, Props}
+import akka.routing.{ActorRefRoutee, RoundRobinRoutingLogic, Router}
+
+object Routers extends App {
+
+
+  // method 1 -- manual router
+  class Master extends Actor {
+    //step one create routes
+    // 5 actor routes based off slaves actor
+    private val slaves = for (_ <- 1 to 5) yield {
+      val slave = context.actorOf(Props[Slave])
+      context.watch(slave)
+      ActorRefRoutee(slave)
+      // slave // TODO
+    }
+    //step two define router
+    private val router = Router(RoundRobinRoutingLogic(), slaves)
+
+    // step route three route message
+    override def receive: Receive = {
+      case message =>
+        router.route(message,sender())
+
+    }
+  }
+
+  class Slave extends Actor with ActorLogging {
+
+    override def receive: Receive = {
+      case message => log.info(message.toString)
+    }
+
+  }
 
 }
